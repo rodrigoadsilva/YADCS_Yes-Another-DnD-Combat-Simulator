9977b12203d972b4bb57831b73af28adf56ee141
Rodrigo da Silva <rodrigo@rodrigoads.com>
2020-10-18 21:19:01 -0300

    Change framework to Bootstrap

--- js/App.js
+++ js/App.js
@@ -1,12 +1,35 @@
 let url = "https://www.dnd5eapi.co/api/races";
 let dataReturn;
 
-/*
-let jqxhr = $.get( url)
-    .done(function(data) {
-      dataReturn = data;
-    })
-    .fail(function() {
-      alert( "error" );
-    });
-    */
+function callRaces(){
+  let callResult;
+  $.ajax({url, async: false})
+  .done(function(data) {
+    dataReturn = data.results;
+    callResult = data.results;
+  })
+  .fail(function() {
+    alert( "error" );
+  });
+  return callResult;
+}
+
+function createSelectorRace(races){
+  let racesSelector = '<option selected>Choose here a race</option>';
+  races.forEach((race) => {
+    racesSelector += `<option value="${race.index}">${race.name}</option>`
+    console.log(race)
+  });
+  $('#characterRaceSelector').html(racesSelector)
+}
+
+function createSelectorClasses(classes){
+
+}
+
+$(document).ready(() => {
+  let races = callRaces();
+  console.log(races)
+  createSelectorClasses(races);
+  
+})

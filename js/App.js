24ecf10d93143ce5eca967697d032fe2ebace003
Rodrigo da Silva <rodrigo@rodrigoads.com>
2020-10-19 22:21:08 -0300

    Created the classes selector and pre-sheet

--- js/App.js
+++ js/App.js
@@ -2,10 +2,10 @@ let url = "https://www.dnd5eapi.co/api/";
 let dataReturn;
 let playerCharacter = {}
 
-function callRaces() {
+function callApi(query) {
     let callResult
     $.get({
-        url: url + 'races',
+        url: url + query,
         async: false
     })
         .done(function (data) {
@@ -21,24 +21,41 @@ function callRaces() {
 function createSelectorRace(races) {
     let racesSelector = '<option selected>Choose here a race</option>';
     races.forEach((race) => {
-        racesSelector += `<option value="${race.index}">${race.name}</option>`
+        racesSelector += `<option value="${race.index}" name="${race.name}">${race.name}</option>`
     });
     $('#characterRaceSelector').html(racesSelector)
 }
 
 function createSelectorClasses(classes) {
-
+    let classesSelector = '<option selected>Choose here a class</option>';
+    classes.forEach((classKey) => {
+        classesSelector += `<option value="${classKey.index}" name="${classKey.name}">${classKey.name}</option>`
+    });
+    $('#characterClassSelector').html(classesSelector)
 }
 
 $(document).ready(() => {
+    let races = callApi('races');
+    createSelectorRace(races);
+
     $('#characterRaceSelector').change(() => {
         let selectorValue = $('#characterRaceSelector').val();
         if(selectorValue !== 'Choose here a race'){
-            console.log(selectorValue);
+            let raceName = $('#characterRaceSelector').find('option:selected').attr("name");
+            $('#raceSelected').html(raceName);
+            let classes = callApi('classes');
+            createSelectorClasses(classes);
             $('#collapseTwo').collapse('toggle');
             $('#collapseOne').collapse('toggle');
         }
-    })
-    let races = callRaces();
-    createSelectorRace(races);
+    });
+    $('#characterClassSelector').change(() => {
+        let selectorValue = $('#characterClassSelector').val();
+        if(selectorValue !== 'Choose here a class'){
+            let className = $('#characterClassSelector').find('option:selected').attr("name");
+            $('#classSelected').html(className);
+            $('#collapseTwo').collapse('toggle');
+        }
+    });
+
 })
